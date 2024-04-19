215abd80c095367ef9dbed9bea7ce29d76fc3db6
Shavi Jayakodi <shavijayakodi2000@gmail.com>
2024-04-19 05:34:32 +0530

    commit

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Container } from '@mui/material';
+import { TextField, Button, Typography, Container, Grid, MenuItem } from '@mui/material';
 
 function Registration() {
   const [formData, setFormData] = useState({
+    email: '',
     firstName: '',
     lastName: '',
-    email: '',
-    message: ''
+    salutation: '',
+    dob: '',
+    street: '',
+    city: '',
+    postalCode: ''
   });
 
   const handleChange = (e) => {
@@ -24,47 +28,103 @@ function Registration() {
   };
 
   return (
-    
     <Container maxWidth="sm">
-
-     <Typography variant="h4" align="center" gutterBottom>
-      Registration
-    </Typography>
+      <Typography variant="h4" align="center" gutterBottom>
+        Registration
+      </Typography>
       <form onSubmit={handleSubmit}>
-        <TextField
-          label="First Name"
-          name="firstName"
-          value={formData.name}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Last Name"
-          name="lastName"
-          value={formData.name}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          fullWidth
-          multiline
-          rows={4}
-          margin="normal"
-        />
+        <Grid container spacing={2}>
+        <Grid item xs={12}>
+            <TextField
+              select
+              label="Salutation"
+              name="salutation"
+              value={formData.salutation}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            >
+              <MenuItem value="Mr.">Mr.</MenuItem>
+              <MenuItem value="Mrs.">Mrs.</MenuItem>
+              <MenuItem value="Miss">Miss</MenuItem>
+            </TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="First Name"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Last Name"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          </Grid>
+          
+          <Grid item xs={6}>
+            <TextField
+              label="Date of Birth"
+              name="dob"
+              type="date"
+              value={formData.dob}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Street"
+              name="street"
+              value={formData.street}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="City"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Postal Code"
+              name="postalCode"
+              value={formData.postalCode}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+          </Grid>
+        </Grid>
         <Button type="submit" variant="contained" color="primary">
           Submit
         </Button>
@@ -74,4 +134,3 @@ function Registration() {
 }
 
 export default Registration;
-
