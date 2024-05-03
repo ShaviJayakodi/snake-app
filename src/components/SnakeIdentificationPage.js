79ffa3a00b02c351442cffef1aee58be77f7a756
Shavi Jayakodi <shavijayakodi2000@gmail.com>
2024-05-03 06:21:31 +0530

    Add first-aid advise

--- src/components/SnakeIdentificationPage.js
+++ src/components/SnakeIdentificationPage.js
@@ -8,6 +8,7 @@ function SnakeIdentification() {
     image: null,
   });
   const [previewImage, setPreviewImage] = useState(null);
+  const [snakeData, setSnakeData] = useState(null);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -20,48 +21,9 @@ function SnakeIdentification() {
     }
   };
 
-  
-  
   const handleCapture = async () => {
-    if ("mediaDevices" in navigator) {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
-        const mediaStreamTrack = stream.getVideoTracks()[0];
-        const imageCapture = new ImageCapture(mediaStreamTrack);
-  
-        const photo = await imageCapture.takePhoto();
-  
-        // Create a Blob object from the captured photo data
-        const blob = new Blob([photo], { type: 'image/jpeg' });
-  
-        setFormData({
-          ...formData,
-          image: blob
-        });
-  
-        setPreviewImage(URL.createObjectURL(blob)); // Set the preview image URL
-  
-        mediaStreamTrack.stop(); // Stop the camera stream
-      } catch (error) {
-        console.error('Error capturing photo:', error);
-        Swal.fire({
-          title: 'Error!',
-          text: 'Failed to capture photo. Please try again.',
-          icon: 'error'
-        });
-      }
-    } else {
-      console.error('getUserMedia not supported');
-      Swal.fire({
-        title: 'Error!',
-        text: 'Your device does not support camera capture.',
-        icon: 'error'
-      });
-    }
+    // ... existing code
   };
-  
-  
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -90,8 +52,51 @@ function SnakeIdentification() {
         }
       })
       .then(function(response) {
-        console.log(response.data);
-        console.log(response.data.predictions[0].class);
+        const classification = response.data.predictions[0].class;
+        axios.get(`http://localhost:8080/snakeData/getSnakesByName?snakeName=${classification}`)
+        .then(function (response) {
+          setSnakeData(response.data.payload);
+          let venomousButton = '';
+          let treat = '';
+            if (response.data.payload.venomousOrNot === "Venomous") {
+              treat = ` <p>
+                          <h3>If Bitten, Please Do:</h3>
+                          - Go to the nearest hospital as all government hospitals possess anti-venom.<br/>
+                          - Apply a bandage on the bite site covering the bite and the area around it.<br/>
+                          - The bandage should not be tight.<br/>
+                          - Avoid unnecessary and sharp movements.<br/>
+                          - Place a splint while bandaging to avoid joint movements.<br/>
+                          - Walking or running is not advisable.<br/>
+                          - In case of a bite on the hand, bandage the arm and put it in a sling from the shoulder.<br/>
+                      </p>`
+              venomousButton = `<br/><button class="swal2-confirm swal2-styled" onclick="window.location.href='/your-venomous-page'">Find a Snake Catcher</button>`;
+            } else{
+              treat = `<p>
+                          <h3>If Bitten, Please Do:</h3>
+                          - Wash the bite area gently with soap and water to prevent infection.<br/>
+                          - If the bite caused any bleeding, apply a clean cloth or bandage to the wound and elevate the affected limb if possible.<br/>
+                          - Use a cold pack or a cloth soaked in cold water to reduce swelling and pain.<br/>
+                          - Keep an eye on the bite area for any signs of infection, such as increased redness, swelling, or pus.<br/>
+                          - If the bite is painful, you can use over-the-counter pain relievers like acetaminophen or ibuprofen.<br/>
+                          - While non-venomous snake bites are usually not serious, it's still a good idea to seek medical attention to ensure proper wound care and to rule out any complications.<br/>
+                      </p>`
+            }
+
+          Swal.fire({
+            title: 'Identification Result',
+            html: `
+              <p>Snake Name: ${response.data.payload.snakeName}</p>
+              <p>Venomous or Not: ${response.data.payload.venomousOrNot}</p>
+              <p>Venomous Level: ${response.data.payload.venomousLevel}</p>
+              ${treat}
+              ${venomousButton}
+            `,
+            
+          });
+        })
+        .catch(function (error) {
+          console.error('Error fetching snake data:', error);
+        });
       })
       .catch(function(error) {
         console.log(error.message);
@@ -109,8 +114,8 @@ function SnakeIdentification() {
         icon: 'error'
       });
     }
-  };
-  
+  }; 
+
   const loadImageBase64 = (file) => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -119,7 +124,6 @@ function SnakeIdentification() {
       reader.readAsDataURL(file instanceof Blob ? file : file[0]);
     });
   };
-
 
   return (
     <Container maxWidth="sm" style={{ background: '#C6EBC5', padding: '15px', marginTop: '25px', borderRadius: '15px', textAlign: 'center', justifyContent: 'center', minHeight: '65vh' }}>
